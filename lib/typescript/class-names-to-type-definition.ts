c7ff942aef3a787a94395fcd98db63ed645d1bac
Charlie Harding <charlie_harding@icloud.com>
2019-07-15 15:54:21 +0200

    Use React Native style types

--- lib/typescript/class-names-to-type-definition.ts
+++ lib/typescript/class-names-to-type-definition.ts
@@ -5,12 +5,14 @@ import { alerts } from "../core";
 
 export type ExportType = "named" | "default";
 export const EXPORT_TYPES: ExportType[] = ["named", "default"];
+export const REACT_NATIVE_IMPORT = `import { StyleProp } from 'react-native';`;
+export const REACT_NATIVE_TYPE = `StyleProp<any>`;
 
 const classNameToNamedTypeDefinition = (className: ClassName) =>
-  `export const ${className}: string;`;
+  `export const ${className}: ${REACT_NATIVE_TYPE};`;
 
 const classNameToInterfaceKey = (className: ClassName) =>
-  `  '${className}': string;`;
+  `  '${className}': ${REACT_NATIVE_TYPE};`;
 
 const isReservedKeyword = (className: ClassName) =>
   reserved.check(className, "es5", true) ||
@@ -37,24 +39,25 @@ export const classNamesToTypeDefinitions = (
   exportType: ExportType
 ): string | null => {
   if (classNames.length) {
-    let typeDefinitions;
-
     switch (exportType) {
-      case "default":
-        typeDefinitions = "export interface Styles {\n";
+      case "default": {
+        let typeDefinitions = `${REACT_NATIVE_IMPORT}\n\n`;
+        typeDefinitions += "export interface Styles {\n";
         typeDefinitions += classNames.map(classNameToInterfaceKey).join("\n");
         typeDefinitions += "\n}\n\n";
         typeDefinitions += "export type ClassNames = keyof Styles;\n\n";
         typeDefinitions += "declare const styles: Styles;\n\n";
         typeDefinitions += "export default styles;\n";
         return typeDefinitions;
-      case "named":
-        typeDefinitions = classNames
+      }
+      case "named": {
+        const typeDefinitions = classNames
           .filter(isValidName)
           .map(classNameToNamedTypeDefinition);
 
-        // Sepearte all type definitions be a newline with a trailing newline.
-        return typeDefinitions.join("\n") + "\n";
+        // Separate all type definitions be a newline with a trailing newline.
+        return `${REACT_NATIVE_IMPORT}\n\n${typeDefinitions.join("\n")}\n`;
+      }
       default:
         return null;
     }
